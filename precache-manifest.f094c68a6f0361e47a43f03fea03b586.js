e820f81e30e88bd5e2b1ea58b2abe37d2fb60b85
Patricia7sp <paty7sp@gmail.com>
2020-04-23 10:47:13 -0300

    Updates

---
+++ precache-manifest.f094c68a6f0361e47a43f03fea03b586.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfd1f2dc0db2c1966b478c414c4b0c98",
+    "url": "/Projeto_HelloWorld/index.html"
+  },
+  {
+    "revision": "f44c4c45e4beb99d61e2",
+    "url": "/Projeto_HelloWorld/static/css/main.ebd43d67.chunk.css"
+  },
+  {
+    "revision": "63bd6cf7bcf4024738e6",
+    "url": "/Projeto_HelloWorld/static/js/2.6273c943.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Projeto_HelloWorld/static/js/2.6273c943.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f44c4c45e4beb99d61e2",
+    "url": "/Projeto_HelloWorld/static/js/main.89535d2a.chunk.js"
+  },
+  {
+    "revision": "da4fb64f0bbe2cb97748",
+    "url": "/Projeto_HelloWorld/static/js/runtime-main.ab5affc6.js"
+  },
+  {
+    "revision": "876a8325687358ce46ec26be29118535",
+    "url": "/Projeto_HelloWorld/static/media/logo.876a8325.svg"
+  }
+]);
